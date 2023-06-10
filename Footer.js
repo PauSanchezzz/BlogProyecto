398575b7ddd399b62864658a576159551bc437b6
Paula Sanchez <pausanchezt12@gmail.com>
2023-06-09 22:45:39 -0500

    added Footer

---
+++ Footer.js
@@ -0,0 +1,5 @@
+const footer = document.querySelector("#footer");
+footer.innerHTML = `    <section class="footer">
+<p class="p-footer">Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
+<img class="logo-footer" src="assets/platzi.png" alt="logo Platzi">
+</section>`;
